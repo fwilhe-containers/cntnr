aa651b3f017dcd23a20c6f455673d533474cad2b
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-03-31 18:47:49 +0200

    Extend demo app

---
+++ README.md
@@ -0,0 +1,7 @@
+## References
+
+- [man 2 unshare](https://man7.org/linux/man-pages/man2/unshare.2.html)
+
+- [man 3 exec](https://man7.org/linux/man-pages/man3/exec.3.html)
+
+https://man7.org/tlpi/code/online/dist/users_groups/ugid_functions.c.html
--- app.c
+++ app.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pwd.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     char hostname[10];
     gethostname(hostname, 10);
-    printf("uid: %d\n", getuid());
+    printf("args    : %d\n", argc);
+    printf("uid     : %d\n", getuid());
+    printf("username: %s\n", getpwuid(getuid())->pw_name);
     printf("hostname: %s\n", hostname);
     return 0;
 }
